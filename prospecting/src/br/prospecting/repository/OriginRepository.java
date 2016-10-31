f63315bf12daf29da2b57224e3aec497888016c8
eli <eli@eli-james>
2016-10-31 05:34:54 -0200

    Origin Repository added.

---
+++ prospecting/src/br/prospecting/repository/OriginRepository.java
@@ -0,0 +1,15 @@
+package br.prospecting.repository;
+
+import java.util.List;
+
+import br.prospecting.bal.Origin;
+
+public interface OriginRepository {
+	
+	/**
+	 * Returns a list of all origins. In this case, it's not necessary to limit the list size, since it's relatively small, because it does not increase too much overtime.
+	 * @return All origins.
+	 */
+	public List<Origin> list();
+	
+}
