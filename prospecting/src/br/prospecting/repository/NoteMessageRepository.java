e1f093cde0bbe75d42b619de48885368253da923
eli <eli@eli-james>
2016-10-31 08:22:15 -0200

    Interface created for repository operations.

---
+++ prospecting/src/br/prospecting/repository/NoteMessageRepository.java
@@ -0,0 +1,46 @@
+package br.prospecting.repository;
+
+import java.util.List;
+
+import br.prospecting.bal.NoteMessage;
+
+
+/**
+ * Interface for note message database operations.
+ * 
+ * @author Eli James
+ *
+ */
+public interface NoteMessageRepository {
+	
+	/**
+	 * Adds a {@link NoteMessage} to a prospect.
+	 * 
+	 * @param noteMessage
+	 * @param prospect
+	 */
+	public void save(NoteMessage noteMessage, long idProspect);
+	
+	
+	/**
+	 * Returns a note message list, with a specific page and limit per page.
+	 * 
+	 * @param page
+	 * @param limit
+	 * @return Note Message List
+	 */
+	public List<NoteMessage> list(int page, int limit);
+	
+	/**
+	 * Remove a note message.
+	 * @param id
+	 */
+	public void remove(long id);
+	
+	/**
+	 * Update a note message. It should not update the post date, for only message correction purpose.
+	 * 
+	 * @param noteMessage
+	 */
+	public void update(NoteMessage noteMessage);
+}
