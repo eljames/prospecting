63bac4a1d38f909d679b963d69e4d8329a92c008
eli <eli@eli-james>
2016-10-31 04:44:59 -0200

    DBException created

---
+++ prospecting/src/br/prospecting/repository/DBException.java
@@ -0,0 +1,19 @@
+package br.prospecting.repository;
+
+
+/**
+ * This class indicates a exception for database operations.
+ * 
+ * @author Eli James
+ *
+ */
+public class DBException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
