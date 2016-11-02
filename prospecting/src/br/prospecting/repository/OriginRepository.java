0269c73724d8f058e6cbb3d900a7bd7e482eaf81
eli <eli@eli-james>
2016-11-01 22:23:33 -0200

    Methods changed

--- prospecting/src/br/prospecting/repository/OriginRepository.java
+++ prospecting/src/br/prospecting/repository/OriginRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import br.prospecting.bal.Origin;
 
-public interface OriginRepository {
+public interface OriginRepository extends GenericRepository<Origin> {
 	
 	/**
 	 * Returns a list of all origins. In this case, it's not necessary to limit the list size, since it's relatively small, because it does not increase too much overtime.
