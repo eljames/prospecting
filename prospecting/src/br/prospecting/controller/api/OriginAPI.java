012e9b3b20478fd5934f3f9900d7dcc95898994c
Eli James <eli@eli-james>
2016-11-03 07:02:22 -0200

    OriginAPI done. All RESTFul API for origin done.

--- prospecting/src/br/prospecting/controller/api/OriginAPI.java
+++ prospecting/src/br/prospecting/controller/api/OriginAPI.java
@@ -3,7 +3,9 @@ package br.prospecting.controller.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,50 @@ public class OriginAPI {
 	@Autowired
 	private OriginService originService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public void getList() throws DBException {
-		List<Origin> list = originService.listOrigins();
+	/**
+	 * POST /origins
+	 * 
+	 * @param origin
+	 * @throws DBException
+	 */
+	@RequestMapping(method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public void save(@RequestBody Origin origin) throws DBException {
+		originService.saveOrigin(origin);
+	}
+	
+	/**
+	 *  GET /origins
+	 * 
+	 * @return origin list
+	 * @throws DBException
+	 */
+	@RequestMapping(method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<Origin> listOrigins() throws DBException {
+		return originService.listOrigins();
+	}
+	
+	/**
+	 *  PUT /origins/{id}
+	 * 
+	 * @param id
+	 * @param origin
+	 * @throws DBException
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public void update(@PathVariable("id") long id, @RequestBody Origin origin) throws DBException {
+		origin.setId(id);
+		originService.updateOrigin(origin);
+	}
+	
+	/**
+	 *  DELETE /origins/{id}
+	 * 
+	 * @param id
+	 * @throws DBException
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void remove(@PathVariable("id") long id) throws DBException {
 		
-		for(Origin origin : list) {
-			System.out.println(origin.getId() + " | " + origin.getName());
-		}
+		originService.removeOrigin(id);
 	}
 }
